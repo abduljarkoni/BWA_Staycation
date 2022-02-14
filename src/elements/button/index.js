59d9667a5eb935e04011c4f03aee2eb57c64da95
abdul jarkoni <abduljarkoni.aj@gmail.com>
2022-02-14 22:06:59 +0700

    bikin button

--- src/elements/button/index.js
+++ src/elements/button/index.js
@@ -1,5 +1,56 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import propTypes from "prop-types";
 
+// 1. function
 export default function Button(props) {
+  //  3. implementasi propTypes
+  const className = [props.className];
+  if (props.isPrimary) className.push("btn-primary");
+  if (props.isLarge) className.push("btn-lg");
+  if (props.isSmall) className.push("btn-sm");
+  if (props.isBlock) className.push("btn-block");
+  if (props.hasShadow) className.push("btn-shadow");
+
+  // 4. function untuk menghandle onclick
+  const onClick = () => {
+    if (props.onClick) props.onClick();
+  };
+
+  // 5. pengecekan link yang digunakan
+  if (props.type === "link") {
+    if (props.isExternal) {
+      return (
+        <a
+          href={props.href}
+          className={className.join(" ")}
+          style={props.style}
+          target={props.target === "_blank" ? "_blank" : undefined}
+          rel={props.target === "_blank" ? "noopener noreferrer" : undefined}
+        >
+          {props.children}
+        </a>
+      );
+    } else {
+      return <Link></Link>;
+    }
+  }
+
   return <div></div>;
 }
+
+// 2 propTypes
+Button.propTypes = {
+  type: propTypes.oneOf(["button", "link"]),
+  onClick: propTypes.func,
+  href: propTypes.string,
+  target: propTypes.string,
+  className: propTypes.string,
+  isDisabled: propTypes.bool,
+  isLoading: propTypes.bool,
+  isSmall: propTypes.bool,
+  isLarge: propTypes.bool,
+  isBlock: propTypes.bool,
+  isExternal: propTypes.bool,
+  hasShadow: propTypes.bool,
+};
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import moduleName from "elements/button/index";
+
+export default class LandingPage extends Component {
+  render() {
+    return <div>LandingPage</div>;
+  }
+}
