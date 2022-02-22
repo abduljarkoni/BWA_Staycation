add063e156ccc82d719497cdef98bfc45aaaf820
abdul jarkoni <abduljarkoni.aj@gmail.com>
2022-02-22 09:10:49 +0700

    logo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from "react";
-import "../src/assets/scss/style.scss";
+import React from 'react'
+import '../src/assets/scss/style.scss'
 
 function App() {
-  return <div className="App">Hai dunia</div>;
+  return <div className="App">Hai dunia</div>
 }
 
-export default App;
+export default App
