532b0823a103e31d3c08d06d5a7ec59ec95fab1a
abdul jarkoni <abduljarkoni.aj@gmail.com>
2022-02-23 10:12:14 +0700

    navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import '../src/assets/scss/style.scss'
+import Header from './parts/Header'
 
 function App() {
-  return <div className="App">Hai dunia</div>
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  )
 }
 
 export default App
---
+++ src/testing.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Testing = () => {
+  return (
+    <div>
+      <h1>
+        Welcome fghfhgfjhgfhjfhgfghfghfghfghdghdgfdfgdgsdfsdsfdsdfsfdsWorld
+      </h1>
+    </div>
+  )
+}
+
+export default Testing
